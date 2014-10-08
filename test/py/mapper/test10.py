8a39488d68c5fabd97941ca7e74a499b0a804d8a
Alberto Paoluzzi <apaoluzzi@me.com>
2014-10-08 20:17:25 +0200

    Some testing of internal boundaries removal done

--- test/py/mapper/test10.py
+++ test/py/mapper/test10.py
@@ -6,7 +6,7 @@ from larcc import *
 from mapper import evalStruct
 
 """ Generation of Struct object and transform to LAR model pair """
-cubes = larCuboids([3,3,3],True)
+cubes = larCuboids([10,10,10],True)
 V = cubes[0]
 FV = cubes[1][-2]
 CV = cubes[1][-1]
@@ -15,7 +15,9 @@ BV = [FV[f] for f in bcells]
 VIEW(EXPLODE(1.2,1.2,1.2)(MKPOLS((V,BV))))
 
 block = Model((V,BV))
-struct = Struct(30*[block, t(3,0,0)])
+struct = Struct(10*[block, t(10,0,0)])
+struct = Struct(10*[struct, t(0,10,0)])
+struct = Struct(3*[struct, t(0,0,10)])
 W,FW = struct2lar(struct)
 
 VIEW(EXPLODE(1.2,1.2,1.2)(MKPOLS((W,FW))))
