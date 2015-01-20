b415e23c920819423a02bb2c03263a5c705fd8f6
Alberto Paoluzzi <apaoluzzi@me.com>
2015-01-20 08:51:52 +0100

    crumbs

--- test/py/hospital/test01.py
+++ test/py/hospital/test01.py
@@ -2,11 +2,8 @@ import sys
 sys.path.insert(0, 'lib/py/')
 from hospital import *
 
-models = evalStruct(copyStruct(groundFloor))
-lars = AA(AS(SEL)([1,3]))(models)
-VIEW(STRUCT(CAT(AA(MKPOLS)(lars))))
+V,FV,EV = struct2lar(groundFloor)
+VIEW(STRUCT(MKPOLS((V,EV))))
 
-Vs,EVs = TRANS(AA(AS(SEL)([1,3]))(models))
-Vs = AA(metric)(Vs)
-lars = TRANS([Vs,EVs])
-VIEW(STRUCT(CAT(AA(MKPOLS)(lars))))
+V,FV,EV = struct2lar(groundFloor,metric)
+VIEW(STRUCT(MKPOLS((V,EV))))
