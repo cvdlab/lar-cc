bacf5c4215facd79a9e8cdef92efd31632a31bb9
Alberto Paoluzzi <apaoluzzi@me.com>
2015-11-13 06:52:20 +0100

    added test of LAR of non-connected 2-complexes

--- larlib/build/lib/larlib/hijson.py
+++ larlib/build/lib/larlib/hijson.py
@@ -76,6 +76,20 @@ def chain2BoundaryChain(FV,EV):
         return boundaryChain
     return chain2BoundaryChain0
 
+def chain2BoundaryChain(FV,EV):
+	csrBoundaryMat = boundary(FV,EV)
+	nedges,nfaces = csrBoundaryMat.shape
+	def chain2BoundaryChain0(chain):
+		row = np.array(range(len(chain)))
+		col = np.array([0 for k in range(len(chain))])
+		data = np.array(chain)
+		csrFaceVect = scipy.sparse.coo_matrix((data, (row, col)), shape=(nfaces,1)).tocsr()
+		csrEdgeVect = csrBoundaryMat*csrFaceVect
+		boundaryChain = [h for h,val in
+			zip(csrEdgeVect.tocoo().row, csrEdgeVect.tocoo().data) if val%2 != 0]
+		return boundaryChain
+	return chain2BoundaryChain0
+
 """ From chains to structures """
 def chain2structs(V,FV,EV,FE):
     def chain2structs0(args): 
--- test/py/inters/test16.py
+++ test/py/inters/test16.py
@@ -1,1 +1,27 @@
-""" Drawing multiply-connected boundary polylines """
+""" Generating the LAR of a set of non-intersecting cycle """
+from larlib import *
+
+sys.path.insert(0, 'test/py/inters/')
+from test15 import *
+
+cells = cellsFromCycles(testArray)
+CV = AA(COMP([list,set,CAT]))(EVs)
+EVdict = dict(zip(EV,range(len(EV))))
+FE = [[EVdict[edge] for edge in cycle] for cycle in EVs] 
+edges = [CAT([FE[cycle] for cycle in cell]) for cell in cells]
+FVs = [[CV[cycle] for cycle in cell] for cell in cells]
+FV = AA(CAT)(FVs)
+
+chains = [[1,0,0]]
+chains += [[0,1,0]]
+chains += [[0,0,1]]
+chains += [[1,1,0]]
+chains += [[0,1,1]]
+chains += [[1,0,1]]
+chains += [[1,1,1]]
+
+cycles = STRUCT(MKPOLS((V,EV)))
+for chain in chains:
+    chainBoundary = COLOR(RED)(STRUCT(MKPOLS((V,[EV[e] 
+                        for e in chain2BoundaryChain(FV,EV)(chain)]))))
+    VIEW(STRUCT([cycles, chainBoundary]))
