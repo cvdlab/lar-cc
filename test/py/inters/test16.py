bb788df4cad33f8d6ad6ca6ff4247ec05f04c22a
Alberto Paoluzzi <apaoluzzi@me.com>
2015-11-15 05:53:29 +0100

    generalized test of non intersecting cycles

--- test/py/inters/test16.py
+++ test/py/inters/test16.py
@@ -12,6 +12,13 @@ edges = [CAT([FE[cycle] for cycle in cell]) for cell in cells]
 FVs = [[CV[cycle] for cycle in cell] for cell in cells]
 FV = AA(CAT)(FVs)
 
+def allBinarySubsetsOfLenght(n):
+   out = [list(('{0:0'+str(n)+'b}').format(k)) for k in range(1,2**n)]
+   return AA(AA(int))(out)
+
+n = len(cells)
+chains = allBinarySubsetsOfLenght(n)
+"""
 chains = [[1,0,0]]
 chains += [[0,1,0]]
 chains += [[0,0,1]]
@@ -19,7 +26,7 @@ chains += [[1,1,0]]
 chains += [[0,1,1]]
 chains += [[1,0,1]]
 chains += [[1,1,1]]
-
+"""
 cycles = STRUCT(MKPOLS((V,EV)))
 for chain in chains:
     chainBoundary = COLOR(RED)(STRUCT(MKPOLS((V,[EV[e] 
