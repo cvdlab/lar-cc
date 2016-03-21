6562bf1fd3d9f54734f51fc0000b8597a3fa8b72
Alberto Paoluzzi <apaoluzzi@me.com>
2016-03-21 06:49:50 +0100

    added some docs

--- larlib/build/lib/larlib/boundary.py
+++ larlib/build/lib/larlib/boundary.py
@@ -234,9 +234,7 @@ def adjFace(boundaryOperator,EV,EF_angle,faceChainOrientation):
         if orientation > 0:  edgeLoop = REVERSE(EF_angle[edge])
         elif orientation < 0:  edgeLoop = EF_angle[edge]
         edgeLoop = edgeLoop + [edgeLoop[0]]  # all positive indices
-        print "edgeLoop =",edgeLoop
         
-        print "faceChainOrientation =",faceChainOrientation
         pivotFace = set([f for f,_ in faceChainOrientation]).intersection(edgeLoop).pop()
         if pivotFace in edgeLoop:
             pivotIndex = edgeLoop.index(pivotFace)
@@ -245,14 +243,11 @@ def adjFace(boundaryOperator,EV,EF_angle,faceChainOrientation):
         adjacentFace = edgeLoop[pivotIndex+1]
         
         theSign = boundaryOperator[edge,adjacentFace]
-        print "adjacentFace,edge,pivotFace,theSign =",adjacentFace,edge,pivotFace,theSign
         return adjacentFace, -(theSign*orientation)
-        #return adjacentFace, theSign
     return adjFace0
 
 """ Choose the start facet in extracting the facet representation of a cell """
 def chooseStartFace(FV,faceCounter):
-    print "\n>>>>>>> ECCOMI"
     for f in range(len(FV)):
         if faceCounter[f,0]==1 and faceCounter[f,1]==0: return (f,-1)
         elif faceCounter[f,0]==0 and faceCounter[f,1]==1: return (f,1)
@@ -271,94 +266,46 @@ def signedBasis(boundaryOperator):
 
 """ Return the signed boundary matrix of a 3-complex """
 def larSignedBoundary3((V,FV,EV)):
-   """
-   sort on angles the co-boundaries of 1-cells (loops of signed faces)
-   """
    model = V,FV,EV
    faceCounter = zeros((len(FV),2),dtype='b')
-   CF = []
+   CF,m = [],len(FV)
    efOp = larFaces2Edges(V,FV,EV)
    FE = [efOp([k]) for k in range(len(FV))]
    EF_angle, _,_,_ = faceSlopeOrdering(model,FE)
-   """
-   initialize the 2-coboundary array of arrays, with boundary 2-faces of 3-cells by row
-   """
-   m = len(FV)
-   nonWorkedFaces = set(range(m))
-   coboundary_2 = []
+   nonWorkedFaces,coboundary_2,cellNumber = set(range(m)),[],0
    boundaryOperator = larSignedBoundary2(V,FV,EV)
    FEbasis = signedBasis(boundaryOperator)
-   cellNumber=0
    row,col,data = [],[],[]
-   """
-   repeat until the set of non-worked faces is empty
-   """
    while True:
-      print "\n>>>>>>> CIAO"
-      """
-      Take an elementary 2-chain F = [f]
-      """
-      #startFace = nonWorkedFaces.pop()
       startFace,orientation = chooseStartFace(FV,faceCounter)
-      print "\n\n*** startFace =",startFace
       if startFace == -1: break
       nonWorkedFaces = nonWorkedFaces.difference({startFace})
       faceChainOrientation = {(startFace,orientation)}
-      """
-      compute the oriented 1-boundary  E := partial_2(F) (loops of signed edges)
-      """
       vect = csc_matrix((m,1),dtype='b')
       for face,orientation in faceChainOrientation:  
           vect[face] = orientation
       edgeCycleCoords = boundaryOperator * vect
       edgeCycle = coords2chain(edgeCycleCoords)
-      print "\nedgeCycle esterno=",edgeCycle
-      """
-      repeat until E[F] = \emptyset
-      """
       while edgeCycle != []:
-         """
-         for each edge on E 
-         """
          look4face = adjFace(boundaryOperator,EV,EF_angle,faceChainOrientation)
          for edge,orientation in edgeCycle:
-            print "\nedge,orientation=",edge,orientation
             adjacentFace,orientation = look4face(edge,orientation)
-            """
-            assemble all the g_i with F in a new 2-chain F := F \cup [g_i]
-            """
             faceChainOrientation = faceChainOrientation.union(
                 [(adjacentFace,orientation)])
-            print "faceChainOrientation=",faceChainOrientation
-            #VIEW(STRUCT(MKTRIANGLES((V,[FV[f] for f in faceChain],EV),color=True)))
             nonWorkedFaces = nonWorkedFaces.difference([adjacentFace])
-            print "nonWorkedFaces=",nonWorkedFaces
-            """
-            """
          vect = csc_matrix((m,1),dtype='b')
          for face,orientation in faceChainOrientation:  
              vect[face] = orientation
          edgeCycleCoords = boundaryOperator * vect
-         print "\nedgeCycle interno pre=",edgeCycle
          edgeCycle = coords2chain(edgeCycleCoords)
-         print "\nedgeCycle interno post=",edgeCycle
-      """
-      put the signed F elements in a new column of $\partial_3$ (new row of coboundary_2)
-      """
       row += [face for face,_ in faceChainOrientation]
       col += [cellNumber for face,orientation in faceChainOrientation]
       data += [orientation for _,orientation in faceChainOrientation]
-      cellNumber += 1
-      
       for face,orientation in faceChainOrientation:
           if orientation == 1: faceCounter[face,0]+=1
           elif orientation == -1: faceCounter[face,1]+=1
-      print "faceChainOrientation =",faceChainOrientation   
-      print "faceCounter =",faceCounter   
-          
       CF += [[face for face,_ in faceChainOrientation]]
-      print "CF =",CF
-
+      cellNumber += 1          
    outMatrix = coo_matrix((data, (row,col)), shape=(m,cellNumber),dtype='b')
    return csr_matrix(outMatrix),CF,faceCounter
 
